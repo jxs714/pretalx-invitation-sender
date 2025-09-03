9cabeb2a124ff951caeed98a7c040e1fbe33f802
jxs714 <jxproductions@gmx.com>
2025-09-03 00:30:24 -0500

    Update urls.py

--- pretalx_invitation_sender/urls.py
+++ pretalx_invitation_sender/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
-
 from .views import InvitationView
 
-# This line is the critical fix. It explicitly creates the namespace
-# that the error message says is missing.
 app_name = "pretalx_invitation_sender"
 
 urlpatterns = [
