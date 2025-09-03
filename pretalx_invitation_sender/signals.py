bcf278f0a957163c2e3765537a6d3726417c944a
jxs714 <jxproductions@gmx.com>
2025-09-03 00:45:08 -0500

    Update signals.py

--- pretalx_invitation_sender/signals.py
+++ pretalx_invitation_sender/signals.py
@@ -1,21 +1,2 @@
-from django.dispatch import receiver
-from django.urls import resolve, reverse
-from django.utils.translation import gettext_lazy as _
-from pretalx.orga.signals import nav_event_settings
-
-@receiver(nav_event_settings)
-def nav_event_settings_invites(sender, request, **kwargs):
-    if not request.user.has_perm("orga.change_settings", request.event):
-        return []
-    url = resolve(request.path_info)
-    return [
-        {
-            "label": _("Send Invites"),
-            "url": reverse(
-                "plugins:pretalx_invitation_sender:send",
-                kwargs={"event": request.event.slug},
-            ),
-            "active": url.namespace == "plugins:pretalx_invitation_sender"
-            and url.url_name == "send",
-        }
-    ]
+# This is a temporary, simplified file for debugging.
+# It contains no code and registers no signals.
