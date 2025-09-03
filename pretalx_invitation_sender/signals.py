c006c0dbbbc84d1cd72d6b8af643c349bc3f26f5
jxs714 <jxproductions@gmx.com>
2025-09-02 21:24:02 -0500

    Update signals.py

--- pretalx_invitation_sender/signals.py
+++ pretalx_invitation_sender/signals.py
@@ -1,26 +1,20 @@
 from django.dispatch import receiver
-from django.urls import resolve, reverse
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
-from pretalx.orga.signals import nav_event_settings
 
+from pretalx.orga.signals import nav_event
 
-@receiver(nav_event_settings)
-def nav_event_settings_invites(sender, request, **kwargs):
-    """
-    Adds a navigation item to the event settings sidebar for sending invitations.
-    """
+@receiver(nav_event)
+def add_nav_item(sender, request, **kwargs):
     url = reverse(
         "plugins:pretalx_invitation_sender:send",
-        kwargs={"event": request.event.slug},
+        kwargs={"event": sender.slug},
     )
-    # Check if the current URL matches the plugin's URL to set the 'active' state
-    is_active = request.resolver_match and request.resolver_match.url_name == "send"
-
     return [
         {
             "label": _("Send Invites"),
             "url": url,
-            "active": is_active,
-            "icon": "paper-plane",
+            "active": request.path == url,
+            "icon": "envelope",
         }
     ]
