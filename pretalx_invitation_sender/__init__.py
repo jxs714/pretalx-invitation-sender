ba9830f355ba956f3404f1e31bf809cc1e312dd4
jxs714 <jxproductions@gmx.com>
2025-09-02 21:22:59 -0500

    Update __init__.py

--- pretalx_invitation_sender/__init__.py
+++ pretalx_invitation_sender/__init__.py
@@ -1,30 +1,10 @@
-from django.apps import AppConfig
-from django.utils.translation import gettext_lazy as _
+[project]
+name = "pretalx-invitation-sender"
+version = "0.1.0"
+description = "Pretalx plugin to send invitation emails to multiple addresses"
+authors = [{name = "Your Name", email = "your@email.com"}]
+license = "Apache-2.0"
+dependencies = ["pretalx >= 2024.0.0"]  # Adjust if your Pretalx version is different
 
-try:
-    from pretalx import __version__ as pretalx_version
-except ImportError:
-    pretalx_version = "0.0.0"
-
-from .__version__ import __version__
-
-
-class PretalxPluginMeta:
-    name = _("Invitation Sender")
-    author = "Your Name"
-    description = _("A plugin to send talk submission invitations to external users.")
-    visible = True
-    version = __version__
-    category = "FEATURE"
-    compatibility = ">=2025.1.0"
-
-
-class PluginApp(AppConfig):
-    name = "pretalx_invitation_sender"
-    verbose_name = "Pretalx Invitation Sender"
-
-    def ready(self):
-        from . import signals
-
-
-default_app_config = "pretalx_invitation_sender.PluginApp"
+[project.entry-points."pretalx.plugin"]
+pretalx_invitation_sender = "pretalx_invitation_sender.apps:InvitationSenderConfig"
