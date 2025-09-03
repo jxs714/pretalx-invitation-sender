e7f9ce0c5c8b29be862a6d856732967a93e39bfd
jxs714 <jxproductions@gmx.com>
2025-09-02 21:51:00 -0500

    Update apps.py

--- pretalx_invitation_sender/apps.py
+++ pretalx_invitation_sender/apps.py
@@ -5,5 +5,13 @@ class InvitationSenderConfig(AppConfig):
     name = "pretalx_invitation_sender"
     verbose_name = _("Invitation Sender")
 
+    class PretalxPluginMeta:
+        name = _("Invitation Sender")
+        author = "Your Name"
+        version = "0.1.0"
+        visible = True
+        description = _("Allows sending invite emails to multiple addresses using existing templates.")
+        category = "CUSTOMIZATION"
+
     def ready(self):
         from . import signals  # NOQA
