2b7dc93923a416e875228d9aa0b76f922a35d339
jxs714 <jxproductions@gmx.com>
2025-09-02 22:06:39 -0500

    Update signals.py

--- pretalx_invitation_sender/signals.py
+++ pretalx_invitation_sender/signals.py
@@ -1,20 +1,31 @@
 from django.dispatch import receiver
-from django.urls import reverse
+from django.urls import resolve, reverse
 from django.utils.translation import gettext_lazy as _
 
-from pretalx.orga.signals import nav_event
+from pretalx.orga.signals import nav_event_settings
 
-@receiver(nav_event)
-def add_nav_item(sender, request, **kwargs):
-    url = reverse(
-        "plugins:pretalx_invitation_sender:send",
-        kwargs={"event": sender.slug},
-    )
+# A helper function that is robust against different URL setups
+def resolve_or_reverse(request, url_name, **kwargs):
+    try:
+        return reverse(url_name, kwargs=kwargs)
+    except Exception:
+        return f"/orga/event/{request.event.slug}/{url_name.split(':')[-1]}/"
+
+
+@receiver(nav_event_settings)
+def nav_event_settings_invites(sender, request, **kwargs):
+    # This function is called by Pretalx to build the settings navigation
+    if not request.user.has_perm("orga.change_settings", request.event):
+        return []
+    
+    # Get the current path to check if our menu item should be "active"
+    url = resolve(request.path_info)
+    
+    # This is the menu item that will be displayed
     return [
         {
             "label": _("Send Invites"),
-            "url": url,
-            "active": request.path == url,
-            "icon": "envelope",
+            "url": resolve_or_reverse(request, "plugins:pretalx_invitation_sender:send", event=request.event.slug),
+            "active": url.namespace == "plugins:pretalx_invitation_sender" and url.url_name == "send",
         }
     ]
