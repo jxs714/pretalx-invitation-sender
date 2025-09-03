63a51d8fb5e509b84677be36b25cbfd093e94725
jxs714 <jxproductions@gmx.com>
2025-09-03 00:30:09 -0500

    Update signals.py

--- pretalx_invitation_sender/signals.py
+++ pretalx_invitation_sender/signals.py
@@ -7,19 +7,15 @@ from pretalx.orga.signals import nav_event_settings
 def nav_event_settings_invites(sender, request, **kwargs):
     if not request.user.has_perm("orga.change_settings", request.event):
         return []
-
-    # Now that urls.py is fixed, this reverse() call will succeed.
-    url_path = reverse(
-        "plugins:pretalx_invitation_sender:send",
-        kwargs={"event": request.event.slug},
-    )
-
-    resolved_url = resolve(request.path_info)
-    
+    url = resolve(request.path_info)
     return [
         {
-            "label": _("Invitation Sender"),
-            "url": url_path,
-            "active": "pretalx_invitation_sender" in resolved_url.namespace,
+            "label": _("Send Invites"),
+            "url": reverse(
+                "plugins:pretalx_invitation_sender:send",
+                kwargs={"event": request.event.slug},
+            ),
+            "active": url.namespace == "plugins:pretalx_invitation_sender"
+            and url.url_name == "send",
         }
     ]
