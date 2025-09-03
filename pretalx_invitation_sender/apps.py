1b044649916cb945b9d8f877b0ed94901c7e6681
jxs714 <jxproductions@gmx.com>
2025-09-02 20:54:30 -0500

    Update apps.py

--- pretalx_invitation_sender/apps.py
+++ pretalx_invitation_sender/apps.py
@@ -8,17 +8,14 @@ class PluginApp(AppConfig):
     name = "pretalx_invitation_sender"
     verbose_name = "A plugin to send talk submission invitations to external users."
 
-    class PretalxPluginMeta:
-        name = _("Invitation Sender")
-        author = "You"
-        description = _("A plugin to send talk submission invitations to external users.")
-        visible = True
-        version = __version__
-        category = "FEATURE"
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
+    def ready(self):
         from . import signals
 
-    def ready(self, *args, **kwargs):
-        super().ready(*args, **kwargs)
+
+class PretalxPluginMeta:
+    name = _("Invitation Sender")
+    author = "Your Name"
+    description = _("A plugin to send talk submission invitations to external users.")
+    visible = True
+    version = __version__
+    category = "FEATURE"
