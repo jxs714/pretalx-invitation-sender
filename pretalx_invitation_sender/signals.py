e9cd2824005c561e0cda0221661452dd033db1b0
jxs714 <jxproductions@gmx.com>
2025-09-03 00:12:19 -0500

    Update signals.py

--- pretalx_invitation_sender/signals.py
+++ pretalx_invitation_sender/signals.py
@@ -1,5 +1,5 @@
 from django.dispatch import receiver
-from django.urls import resolve
+from django.urls import resolve, reverse
 from django.utils.translation import gettext_lazy as _
 from pretalx.orga.signals import nav_event_settings
 
@@ -8,11 +8,11 @@ def nav_event_settings_invites(sender, request, **kwargs):
     if not request.user.has_perm("orga.change_settings", request.event):
         return []
 
-    # THE DEFINITIVE FIX:
-    # Manually construct the correct URL path for plugins.
-    # The previous versions were wrong and failed silently.
-    # This URL structure is guaranteed to work.
-    url_path = f"/orga/events/{request.event.slug}/p/pretalx_invitation_sender/"
+    # Now that urls.py is fixed, this reverse() call will succeed.
+    url_path = reverse(
+        "plugins:pretalx_invitation_sender:send",
+        kwargs={"event": request.event.slug},
+    )
 
     resolved_url = resolve(request.path_info)
     
