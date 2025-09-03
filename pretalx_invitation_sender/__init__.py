c7f821058f38c0e42f12b594f14fe491a7457b08
jxs714 <jxproductions@gmx.com>
2025-09-02 21:09:49 -0500

    Update __init__.py

--- pretalx_invitation_sender/__init__.py
+++ pretalx_invitation_sender/__init__.py
@@ -4,14 +4,6 @@ from django.utils.translation import gettext_lazy as _
 from .__version__ import __version__
 
 
-class PluginApp(AppConfig):
-    name = "pretalx_invitation_sender"
-    verbose_name = "A plugin to send talk submission invitations to external users."
-
-    def ready(self):
-        from . import signals
-
-
 class PretalxPluginMeta:
     name = _("Invitation Sender")
     author = "Your Name"
@@ -19,3 +11,11 @@ class PretalxPluginMeta:
     visible = True
     version = __version__
     category = "FEATURE"
+
+
+class PluginApp(AppConfig):
+    name = "pretalx_invitation_sender"
+    verbose_name = "A plugin to send talk submission invitations to external users."
+
+    def ready(self):
+        from . import signals
