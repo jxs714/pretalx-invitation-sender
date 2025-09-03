9752ec5a01ad3905948c8ff44c4baa50b59005cd
jxs714 <jxproductions@gmx.com>
2025-09-02 21:51:20 -0500

    Update init.py

--- pretalx_invitation_sender/init.py
+++ pretalx_invitation_sender/init.py
@@ -1,9 +1,3 @@
 from .apps import InvitationSenderConfig
 
-class PretalxPluginMeta(InvitationSenderConfig):
-    name = _("Invitation Sender")
-    author = "Your Name"
-    version = "0.1.0"
-    visible = True
-    description = _("Allows sending invite emails to multiple addresses using existing templates.")
-    category = "CUSTOMIZATION"
+PretalxPluginMeta = InvitationSenderConfig.PretalxPluginMeta
